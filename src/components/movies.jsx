81427c7ec36566bca16a254e8e72c1b311ea7e87
njugunapm233 <njugunapm23@gmail.com>
2021-10-01 15:27:46 +0300

    Component design

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -76,7 +76,7 @@ class Movies extends Component {
 
     return (
       <div className="row">
-        <div className="col-3">
+        <div className="col-4">
           <ListGroup
             items={this.state.genres}
             selectedItem={this.state.selectedGenre}
