f97532cfd5349de828c7fc93e0656b6f0ec18fe3
njugunapm233 <njugunapm23@gmail.com>
2021-10-06 09:04:40 +0300

    react routing

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
           <Route path="/rentals" component={Rentals}></Route>
           <Route path="/notfound" component={NotFound}></Route>
           <Redirect from="/" exact to="/movies" />
-          <Redirect to="/notfound" />
+          <Redirect to="/movies" />
         </Switch>
       </main>
     );
